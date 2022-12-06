6502da804cc84b36284c3384d88f6d0195ca9d10
CatThingy <andyc.andychen@gmail.com>
2022-12-05 21:19:08 -0800

    Day 6 part 1

---
+++ day-6/src/main.rs
@@ -0,0 +1,35 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let input = std::io::stdin();
+    let mut buf = String::new();
+    let mut count = 4;
+    let Ok(_) = input.read_line(&mut buf) else { return; };
+    let chars = buf.chars();
+
+    let mut current = chars.take(4).collect::<VecDeque<char>>();
+
+    for char in buf.chars().skip(4) {
+        if confirm_uniqueness(&current) {
+            break;
+        }
+        current.pop_front();
+        current.push_back(char);
+        count += 1;
+    }
+    println!("{}", count);
+}
+
+fn confirm_uniqueness(input: &VecDeque<char>) -> bool {
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            if i == j {
+                continue;
+            }
+            if input[i] == input[j] {
+                return false;
+            }
+        }
+    }
+    return true;
+}
