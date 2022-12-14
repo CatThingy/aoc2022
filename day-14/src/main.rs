722c57a0d86d60f7d0f297d92b86aaed0263bdb2
CatThingy <andyc.andychen@gmail.com>
2022-12-13 22:11:34 -0800

    Day 14 part 2

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -1,12 +1,13 @@
-use std::ops::{Add, AddAssign, Sub, SubAssign};
+use std::{
+    collections::HashMap,
+    ops::{Add, AddAssign, Sub, SubAssign},
+};
 
 const SAND_MOVEMENTS: [Coord; 3] = [Coord::new(0, 1), Coord::new(-1, 1), Coord::new(1, 1)];
 fn main() {
     let input = std::io::stdin();
     let mut rock_paths = Vec::<Vec<Coord>>::new();
 
-    let mut min_x = 20000;
-    let mut max_x = 0;
     let mut max_y = 0;
 
     for line in input.lines() {
@@ -20,8 +21,6 @@ fn main() {
             let coord = coord.split_once(',').unwrap();
             let coord = Coord::new(coord.0.parse().unwrap(), coord.1.parse().unwrap());
 
-            min_x = min_x.min(coord.x);
-            max_x = max_x.max(coord.x);
             max_y = max_y.max(coord.y);
 
             path.push(coord);
@@ -29,12 +28,11 @@ fn main() {
 
         rock_paths.push(path);
     }
-    let width = max_x - min_x + 1;
-    let height = max_y + 1;
+    let height = max_y + 2;
 
-    dbg!((min_x, max_x));
-    dbg!((width, height));
-    let mut map = vec![Tile::Empty; (width * height) as usize];
+    dbg!(height);
+
+    let mut map = HashMap::<Coord, Tile>::new();
 
     for path in rock_paths {
         let mut iter = path.windows(2);
@@ -43,21 +41,19 @@ fn main() {
                 let larger = a.y.max(b.y);
                 let smaller = a.y.min(b.y);
                 for y in smaller..=larger {
-                    map[(y * width + a.x - min_x) as usize] = Tile::Stone;
+                    map.insert(Coord::new(a.x, y), Tile::Stone);
                 }
             } else if a.y == b.y {
                 let larger = a.x.max(b.x);
                 let smaller = a.x.min(b.x);
                 for x in smaller..=larger {
-                    map[(a.y * width + x - min_x) as usize] = Tile::Stone;
+                    map.insert(Coord::new(x, a.y), Tile::Stone);
                 }
             } else {
                 unreachable!()
             }
         }
     }
-
-    // draw_map(&map, width as usize, height as usize);
 
     'a: loop {
         let mut sand_coord = Coord::new(500, 0);
@@ -66,36 +62,53 @@ fn main() {
             let mut stopped = true;
             for direction in SAND_MOVEMENTS {
                 let next_pos = sand_coord + direction;
-                if let Some(tile) = map.get((next_pos.y * width + next_pos.x - min_x) as usize) {
+                if next_pos.y >= height {
+                    break;
+                }
+                if let Some(tile) = map.get(&next_pos) {
                     if tile == &Tile::Empty {
                         sand_coord += direction;
                         stopped = false;
                         break;
                     }
                 } else {
-                    break 'a;
+                    sand_coord += direction;
+                    stopped = false;
+                    break;
                 }
             }
             if stopped {
                 break 'b;
             }
         }
-        map[(sand_coord.y * width + sand_coord.x - min_x) as usize] = Tile::Sand;
+        map.insert(sand_coord, Tile::Sand);
+
+        if sand_coord == Coord::new(500, 0) {
+            break 'a;
+        }
     }
 
-    draw_map(&map, width as usize, height as usize);
+    let mut min_x = 20000;
+    let mut max_x = 0;
 
-    let count = map.into_iter().filter(|v| v == &Tile::Sand).count();
+    for (coord, _) in map.iter() {
+        min_x = min_x.min(coord.x);
+        max_x = max_x.max(coord.x);
+    }
+
+    draw_map(&map, min_x, max_x, height);
+
+    let count = map.into_iter().filter(|(_, v)| v == &Tile::Sand).count();
     println!("{count}");
 }
 
-fn draw_map(map: &[Tile], width: usize, height: usize) {
+fn draw_map(map: &HashMap<Coord, Tile>, min_x: i32, max_x: i32, height: i32) {
     for y in 0..height {
-        for x in 0..width {
-            let output = match map[y * width + x] {
-                Tile::Empty => '.',
-                Tile::Stone => '█',
-                Tile::Sand => 'o',
+        for x in min_x..=max_x {
+            let output = match map.get(&Coord::new(x, y)) {
+                Some(Tile::Empty) | None => '.',
+                Some(Tile::Stone) => '█',
+                Some(Tile::Sand) => 'o',
             };
 
             print!("{output}");
@@ -111,7 +124,7 @@ enum Tile {
     Sand,
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Hash)]
 struct Coord {
     x: i32,
     y: i32,
