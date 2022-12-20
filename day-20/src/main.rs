9304ca8361f0a60f01363f100d96af309c895124
CatThingy <andyc.andychen@gmail.com>
2022-12-19 21:50:23 -0800

    Day 20 part 2

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -3,7 +3,8 @@ fn main() {
 
     let mut encrypted = input
         .lines()
-        .filter_map(|v| v.map(|v| v.parse::<i32>().ok()).ok().flatten())
+        .filter_map(|v| v.map(|v| v.parse::<i64>().ok()).ok().flatten())
+        .map(|v| v * 811589153)
         .collect::<Vec<_>>();
 
     let mut indices = Vec::with_capacity(encrypted.len());
@@ -11,52 +12,50 @@ fn main() {
         indices.push(i);
     }
 
-    let mut current_index = 0;
+    for _ in 0..10 {
+        let mut current_index = 0;
 
-    while current_index < encrypted.len() {
-        let (move_index, _) = indices
-            .iter()
-            .enumerate()
-            .find(|v| *v.1 == current_index)
-            .unwrap();
-        let value = encrypted[move_index];
-        move_element(&mut encrypted, move_index, value);
-        move_element(&mut indices, move_index, value);
+        while current_index < encrypted.len() {
+            let (move_index, _) = indices
+                .iter()
+                .enumerate()
+                .find(|v| *v.1 == current_index)
+                .unwrap();
+            let value = encrypted[move_index];
+            move_element(&mut encrypted, move_index, value);
+            move_element(&mut indices, move_index, value);
 
-        current_index += 1;
+            current_index += 1;
+        }
     }
-
     let (zero_index, _) = encrypted.iter().enumerate().find(|v| *v.1 == 0).unwrap();
 
-    let mut acc = 0;
+    let mut acc: i64 = 0;
 
     for i in 1..=3 {
-        let mut new_index = zero_index as i32 + i * 1000;
+        let mut new_index = zero_index as i64 + i * 1000;
 
-        while new_index < 0 {
-            new_index = encrypted.len() as i32 + new_index;
+        new_index = new_index % encrypted.len() as i64;
+        if new_index < 0 {
+            new_index = encrypted.len() as i64 + new_index;
         }
 
-        while new_index > encrypted.len() as i32 {
-            new_index = new_index - encrypted.len() as i32;
-        }
-        acc += dbg!(encrypted[new_index as usize]);
+        acc += encrypted[new_index as usize] as i64;
     }
 
-    dbg!(acc);
+    println!("{acc}");
 }
 
-fn move_element<T>(list: &mut Vec<T>, index: usize, amount: i32) {
+fn move_element<T>(list: &mut Vec<T>, index: usize, amount: i64) {
     let removed = list.remove(index);
 
-    let mut new_index = index as i32 + amount;
+    let mut new_index = index as i64 + amount;
 
-    while new_index < 0 {
-        new_index = list.len() as i32 + new_index;
+    new_index = new_index % list.len() as i64;
+
+    if new_index < 0 {
+        new_index = list.len() as i64 + new_index;
     }
 
-    while new_index > list.len() as i32 {
-        new_index = new_index - list.len() as i32;
-    }
     list.insert(new_index as usize, removed);
 }
